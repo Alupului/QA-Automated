f2ffa4276a77be2f362115ea29ce11090af0c9c7
Alupului Teodor-Iulian <93911556+Alupului@users.noreply.github.com>
2024-02-01 15:17:53 +0200

    New Adds

--- src/main/java/org/Automation/Steps/GoogleStepsDefinition.java
+++ src/main/java/org/Automation/Steps/GoogleStepsDefinition.java
@@ -1,5 +1,32 @@
 package org.Automation.Steps;
 
 public class GoogleStepsDefinition {
+    GoogleStepsDefinition googleStepsDefinition = new GoogleStepsDefinition();
 
+    public void i_enter_first_page_on_demoqa(){
+        googleStepsDefinition.i_enter_first_page_on_demoqa();
+    }
+
+    public void i_enter_first_page_on_herokuapp(){
+        googleStepsDefinition.i_enter_first_page_on_herokuapp();
+    }
+
+    public void iselectanelemmentfromdemoqa(){
+        googleStepsDefinition.iselectanelemmentfromdemoqa();
+    }
+    public void iselectanelemmentfromherokuapp(){
+        googleStepsDefinition.iselectanelemmentfromherokuapp();
+    }
+    public void iamdirectedtodemoqapageselected(){
+        googleStepsDefinition.iamdirectedtodemoqapageselected();
+    }
+    public void  iamdirectedtoherojuapppageselected(){
+    googleStepsDefinition.iamdirectedtoherojuapppageselected();
+    }
+    public void iamabletoselectansubpagefromdemoqa(){
+        googleStepsDefinition.iamabletoselectansubpagefromdemoqa();
+    }
+    public void iamabletoselectansubpagefromdherokuapp(){
+        googleStepsDefinition.iamabletoselectansubpagefromdherokuapp();
+    }
 }
--- src/main/java/org/Automation/utils/schedule.feature
+++ src/main/java/org/Automation/utils/schedule.feature
@@ -1,12 +1,17 @@
 Feature: Testing schedule feature
 
 
-  Scenario:
-    Given
-    When
-    When
-    Then
+  Scenario: DemoQA
+    Given I enter first page on DemoQA
+    When I select an element from DemoQA
+    When I am directed to DemoQA page selected
+    Then I am able to select an subpage from DemoQA
 
+  Scenario: Herokuapp
+    Given I enter first page on Herokuapp
+  When I select an element from Herokuapp
+  When I am directed to Herokuapp page selected
+  Then I am able to select an subpage from Herokuapp
 
 
 
