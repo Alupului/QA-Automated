129d0ce8d8279a1730acbc3db4eb73bf02b5bdcd
Alupului Teodor-Iulian <93911556+Alupului@users.noreply.github.com>
2024-02-05 14:00:51 +0200

    New Adds

---
+++ src/main/java/Steps/GoogleStepsDefinition.java
@@ -0,0 +1,38 @@
+package Steps;
+
+public class GoogleStepsDefinition {
+    GoogleStepsDefinition googleStepsDefinition = new GoogleStepsDefinition();
+
+        @Given("I enter first page on DemoQA")
+        public void i_enter_first_page_on_demoqa(){
+        googleStepsDefinition.i_enter_first_page_on_demoqa();
+        }
+        @When("I select an element from DemoQA")
+        public void iselectanelemmentfromdemoqa(){
+        googleStepsDefinition.iselectanelemmentfromdemoqa();
+        }
+        @When("^I am directed to DemoQA page selected")
+        public void iamdirectedtodemoqapageselected(){
+        googleStepsDefinition.iamdirectedtodemoqapageselected();
+        }
+        @Then("^I am able to select an subpage from DemoQA")
+        public void iamabletoselectansubpagefromdemoqa(){
+        googleStepsDefinition.iamabletoselectansubpagefromdemoqa();
+        }
+        @Given("I enter first page on Herokuapp")
+        public void i_enter_first_page_on_herokuapp(){
+        googleStepsDefinition.i_enter_first_page_on_herokuapp();
+        }
+        @When("^I select an element from Herokuapp"}
+        public void iselectanelemmentfromherokuapp(){
+        googleStepsDefinition.iselectanelemmentfromherokuapp();
+        }
+        @When("^I am directed to Herokuapp page selected")
+        public void  iamdirectedtoherojuapppageselected(){
+        googleStepsDefinition.iamdirectedtoherojuapppageselected();
+        }
+        @Then("^I am able to select an subpage from Herokuapp")
+        public void iamabletoselectansubpagefromdherokuapp(){
+        googleStepsDefinition.iamabletoselectansubpagefromdherokuapp();
+        }
+}
--- src/main/java/org/Automation/utils/schedule.feature
+++ src/main/java/org/Automation/utils/schedule.feature
@@ -1,12 +1,12 @@
 Feature: Testing schedule feature
 
-
+@Test
   Scenario: DemoQA
     Given I enter first page on DemoQA
     When I select an element from DemoQA
     When I am directed to DemoQA page selected
     Then I am able to select an subpage from DemoQA
-
+@Test
   Scenario: Herokuapp
     Given I enter first page on Herokuapp
     When I select an element from Herokuapp
